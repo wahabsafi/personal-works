8fd8293603a00ef6d29aeac1270db61e69ca9e47
wahabsafi <wahab.safi3@gmail.com>
2021-04-10 23:34:38 +0430

    created room model and created some AbstractDevice and CCTV model

--- building_security_monitor/complex/models.py
+++ building_security_monitor/complex/models.py
@@ -7,54 +7,68 @@ from family.models import Family
 
 
 class Complex(models.Model):
-    name=models.CharField(_("complex name"), max_length=50)
-    address=models.CharField(_("complex address"), max_length=50)
-    
-    class Meta:
-        verbose_name = _("complex")
-        verbose_name_plural = _("complexs")
+	name=models.CharField(_("complex name"), max_length=50)
+	address=models.CharField(_("complex address"), max_length=50)
+	
+	class Meta:
+		verbose_name = _("complex")
+		verbose_name_plural = _("complexs")
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
-    def get_absolute_url(self):
-        return reverse("complex_detail", kwargs={"pk": self.pk})
+	def get_absolute_url(self):
+		return reverse("complex_detail", kwargs={"pk": self.pk})
 
 
 class Block(models.Model):
-    complex=models.ForeignKey(Complex, verbose_name=_("complex"), on_delete=models.CASCADE)
-    no=models.IntegerField(_("block number"))
+	complex=models.ForeignKey(Complex, verbose_name=_("complex"), on_delete=models.CASCADE)
+	no=models.IntegerField(_("block number"))
 
-    class Meta:
-        verbose_name = _("block")
-        verbose_name_plural = _("blocks")
+	class Meta:
+		verbose_name = _("block")
+		verbose_name_plural = _("blocks")
 
-    def __str__(self):
-        return str(self.no)
+	def __str__(self):
+		return str(self.no)
 
-    def get_absolute_url(self):
-        return reverse("block_detail", kwargs={"pk": self.pk})
+	def get_absolute_url(self):
+		return reverse("block_detail", kwargs={"pk": self.pk})
 
 
 class Unit(models.Model):
-    owner=models.OneToOneField(User, verbose_name=_("owner"),null=True,blank=True, on_delete=models.SET_NULL)
-    owner_family=models.OneToOneField(Family, verbose_name=_("familly"),null=True,blank=True, on_delete=models.SET_NULL)
-    phone=models.CharField(_("unit phone number"), max_length=11)
-    block=models.ForeignKey(Block, verbose_name=_("block"), on_delete=models.CASCADE)
-    no=models.IntegerField(_("unit number"))
-    
-    
-    class Meta:
-        verbose_name = _("Unit")
-        verbose_name_plural = _("Units")
+	owner=models.OneToOneField(User, verbose_name=_("owner"),null=True,blank=True, on_delete=models.SET_NULL)
+	owner_family=models.OneToOneField(Family, verbose_name=_("familly"),null=True,blank=True, on_delete=models.SET_NULL)
+	phone=models.CharField(_("unit phone number"), max_length=11)
+	block=models.ForeignKey(Block, verbose_name=_("block"), on_delete=models.CASCADE)
+	no=models.IntegerField(_("unit number"))
+	
+	
+	class Meta:
+		verbose_name = _("Unit")
+		verbose_name_plural = _("Units")
 
-    def __str__(self):
-        if self.owner:
-            owner_full_name=self.owner.get_full_name()
-            info='unit number %i , %s'%(self.no,owner_full_name)
-            return info
-        return 'unit number %i'%(self.no)
+	def __str__(self):
+		if self.owner:
+			owner_full_name=self.owner.get_full_name()
+			info='unit number %i , %s'%(self.no,owner_full_name)
+			return info
+		return 'unit number %i'%(self.no)
 
-    def get_absolute_url(self):
-        return reverse("Unit_detail", kwargs={"pk": self.pk})
+	def get_absolute_url(self):
+		return reverse("Unit_detail", kwargs={"pk": self.pk})
 
+class Room(models.Model):
+	"""Model definition for Room."""
+
+	unit = models.ForeignKey(Unit,null=True, on_delete=models.SET_NULL)
+
+	class Meta:
+		"""Meta definition for Room."""
+
+		verbose_name = 'Room'
+		verbose_name_plural = 'Rooms'
+
+	def __str__(self):
+		"""Unicode representation of Room."""
+		pass
--- building_security_monitor/smart_objects/models.py
+++ building_security_monitor/smart_objects/models.py
@@ -1,21 +1,56 @@
 from django.db import models
+import uuid
 from django.utils.translation import gettext_lazy as _
+class AbstractDevice(models.Model):
+	"""Model definition for AbstractDevice."""
+
+	device_model = models.CharField(_('Model'), max_length=50)
+	manufacture = models.CharField(_('Manufacture'), max_length=50)
+	created_date = models.DateField(_('Build date'), auto_now_add=True)
+	warranty_date = models.DateField(_('Warranty exp date'), auto_now=False, auto_now_add=False)
+	has_warranty = models.BooleanField()
+	device_uuid = models.UUIDField(uniqe=True,primary_key=True,default=uuid.uuid4,editable=False)
+	ip_addr = models.GenericIPAddressField(_('Ip address'), protocol='both', unpack_ipv4=False)
+	class Meta:
+		"""Meta definition for AbstractDevice."""
+		abstract=True
+		verbose_name = 'AbstractDevice'
+		verbose_name_plural = 'AbstractDevices'
+
+	def __str__(self):
+		"""Unicode representation of AbstractDevice."""
+		pass
 
 # Create your models here.
-class Lock(models.Model):
-    """Model definition for Lock."""
+class Lock(AbstractDevice):
+	"""Model definition for Lock."""
 
-    is_open=models.BooleanField(_("open status"))
-    last_opened=models.TimeField(_("last time opened"), auto_now=True)
-    password=models.CharField(_("lock password"), max_length=50)
-    
+	is_open=models.BooleanField(_("open status"))
+	last_opened=models.TimeField(_("last time opened"), auto_now=True)
+	password=models.CharField(_("lock password"), max_length=50)
+	
+	
 
-    class Meta:
-        """Meta definition for Lock."""
+	class Meta:
+		"""Meta definition for Lock."""
 
-        verbose_name = 'Lock'
-        verbose_name_plural = 'Locks'
+		verbose_name = 'Lock'
+		verbose_name_plural = 'Locks'
 
-    def __str__(self):
-        """Unicode representation of Lock."""
-        pass
+	def __str__(self):
+		"""Unicode representation of Lock."""
+		pass
+class CCTV(AbstractDevice):
+	"""Model definition for CCTV."""
+
+	# TODO: Define fields here
+
+	class Meta:
+		"""Meta definition for CCTV."""
+
+		verbose_name = 'CCTV'
+		verbose_name_plural = 'CCTVs'
+
+	def __str__(self):
+		"""Unicode representation of CCTV."""
+		pass
